cc0d5987632932f0e398a9c8df68d0dd2562cf06
pcunha29 <pcunhadev@gmail.com>
2024-04-23 20:01:53 +0100

    first commit

---
+++ src/data/techStack.js
@@ -0,0 +1,70 @@
+import cssLogo from "@/assets/icons/css-3.svg";
+import htmlLogo from "@/assets/icons/html-5.svg";
+import jsLogo from "@/assets/icons/javascript.svg";
+import reactLogo from "@/assets/icons/react.svg";
+import nextLogo from "@/assets/icons/nextjs-icon.svg";
+import tailwindLogo from "@/assets/icons/tailwindcss-icon.svg";
+import vueLogo from "@/assets/icons/vue.svg";
+
+export const techStack = [
+  [
+    {
+      name: "React",
+      logo: reactLogo,
+    },
+    {
+      name: "Next.js",
+      logo: nextLogo,
+    },
+    {
+      name: "TailwindCSS",
+      logo: tailwindLogo,
+    },
+    {
+      name: "Javascript",
+      logo: jsLogo,
+    },
+    {
+      name: "Vue",
+      logo: vueLogo,
+    },
+    {
+      name: "HTML",
+      logo: htmlLogo,
+    },
+    {
+      name: "CSS",
+      logo: cssLogo,
+    },
+  ],
+  [
+    {
+      name: "React",
+      logo: reactLogo,
+    },
+    {
+      name: "Next.js",
+      logo: nextLogo,
+    },
+    {
+      name: "TailwindCSS",
+      logo: tailwindLogo,
+    },
+    {
+      name: "Javascript",
+      logo: jsLogo,
+    },
+    {
+      name: "Vue",
+      logo: vueLogo,
+    },
+    {
+      name: "HTML",
+      logo: htmlLogo,
+    },
+    {
+      name: "CSS",
+      logo: cssLogo,
+    },
+  ],
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,30 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#160F21",
+        secondary: "#1B210F",
+        font: "#99A18A",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 20s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
 };
+
+
+
